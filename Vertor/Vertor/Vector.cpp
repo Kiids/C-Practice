99a843208634d7f0dc6433dfffd94b185d354e9b
Kiids <1980774293@qq.com>
2019-10-15 23:36:18 +0800

    vertor

---
+++ Vertor/Vertor/Vector.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "vertor.h"
+#include <stdlib.h>
+using namespace zsq;
+
+int main()
+{
+	test_vector1();
+	test_vector2();
+
+	system("pause");
+	return 0;
+}
